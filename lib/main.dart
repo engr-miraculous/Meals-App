6f33a9874afd9b349edb67f4ab265bf95eb82daa
MIRACLE <31395674+engr-miraculous@users.noreply.github.com>
2020-02-19 21:27:20 +0100

    Completed appbar tabs, and wanted to change it to bottom Tabbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'category_screen.dart';
+
+import './screens/meal_details_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/category_meals_screen.dart';
+
 
 void main() => runApp(MyApp());
 
@@ -8,28 +12,30 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        accentColor: Colors.amber,
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        fontFamily: "Raleway",
-        textTheme: ThemeData.light().textTheme.copyWith(
-              body1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: "Raleway",
+          textTheme: ThemeData.light().textTheme.copyWith(
+                body1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                body2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                title: TextStyle(
+                  fontFamily: 'RobotoCondensed',
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              body2: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              title: TextStyle(
-                fontFamily: 'RobotoCondensed',
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-      ),
-      home: CategoryScreen(),
-    );
+        ),
+        home: TabsScreen(),
+        routes: {
+          CategoryMealsScreen.namedNav: (ctx) => CategoryMealsScreen(),
+          MealDetailsScreen.navName: (ctx) => MealDetailsScreen(),
+        });
   }
 }
-
